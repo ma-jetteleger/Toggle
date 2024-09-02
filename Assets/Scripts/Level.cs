85ce024aa6a13c6acf95a32f704f0396e1d8ed80
ma-jetteleger <ma.jetteleger@gmail.com>
2024-09-02 15:02:29 -0400

    Rudimentary array is set up, squares are interactable
    There is some feedback on hovering and on clicking the square. It seemed intuitive to implement both in terms of basic interactivity with the components but I'm not sure if there is going to be any value in the hovered feedback in the long run

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,18 +1,81 @@
+using Shapes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private GameObject _squareTemplate = null;
+    [SerializeField] private Vector2Int _squaresRange = Vector2Int.zero;
+    [SerializeField] private float _squaresDistance = 0f;
+
+    private Square _previousHoveredSquare;
+    private Rectangle _squareTemplateRectangle;
+    private Square _lastSquareClickedDown;
+
+	private void Awake()
+	{
+        _squareTemplateRectangle = _squareTemplate.GetComponent<Rectangle>();
+
+        _squareTemplate.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+	private void Start()
+	{
+		var squares = Random.Range(_squaresRange.x, _squaresRange.y + 1);
+
+		for (var i = 0; i < squares; i++)
+		{
+			var newSquare = Instantiate(_squareTemplate, _squareTemplate.transform.parent).GetComponent<Square>();
+            newSquare.transform.position = Vector3.right * (_squareTemplateRectangle.Width + _squaresDistance) * i;
+
+            newSquare.Initialize(i);
+
+            newSquare.gameObject.SetActive(true);
+
+        }
+
+        transform.position = -(Vector3.right * (_squareTemplateRectangle.Width + _squaresDistance) * (squares - 1)) / 2f;
+
+    }
+
+    private void Update()
     {
+        if(_lastSquareClickedDown == null)
+		{
+            var squareHovered = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero).collider?.GetComponent<Square>();
+
+            if (squareHovered != null && squareHovered != _previousHoveredSquare)
+            {
+                if (!squareHovered.Highlighted)
+                {
+                    squareHovered.OnMouseOverEnter();
+                }
+            }
+
+            if (_previousHoveredSquare != null && _previousHoveredSquare.Highlighted && squareHovered != _previousHoveredSquare)
+            {
+                _previousHoveredSquare.OnMouseOverExit();
+            }
+
+            _previousHoveredSquare = squareHovered;
+
+            if (squareHovered != null)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    squareHovered.OnMouseClickDown();
+
+                    _lastSquareClickedDown = squareHovered;
+                }
+            }
+        }           
         
+        if (Input.GetMouseButtonUp(0) && _lastSquareClickedDown != null)
+        {
+            _lastSquareClickedDown.OnMouseClickUp();
+
+            _lastSquareClickedDown = null;
+        }
     }
 }
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -1,18 +1,55 @@
+using Shapes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Square : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private GameObject _outline = null;
+    [SerializeField] private Color _clickedColor = Color.black;
+
+    public bool Highlighted { get; set; }
+
+    private int _id;
+    private Color _normalColor;
+    private Rectangle _rectangle;
+
+	private void Awake()
+	{
+        _rectangle = GetComponent<Rectangle>();
+        _normalColor = _rectangle.Color;
     }
 
-    // Update is called once per frame
-    void Update()
+	public void Initialize(int id)
+	{
+        _id = id;
+
+        _outline.SetActive(false);
+
+        gameObject.name = $"Square({_id})";
+    }
+
+    public void OnMouseOverEnter()
     {
-        
+        _outline.SetActive(true);
+
+        Highlighted = true;
+    }
+
+    public void OnMouseOverExit()
+    {
+        _outline.SetActive(false);
+
+        Highlighted = false;
+    }
+
+    public void OnMouseClickDown()
+	{
+        _rectangle.Color = _clickedColor;
+    }
+
+    public void OnMouseClickUp()
+    {
+        _rectangle.Color = _normalColor;
     }
 }
